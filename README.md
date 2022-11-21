ec3311156b01d0db0573540852bf375e136909dd
Mallory Joki <maljoki@pdx.edu>
2022-11-21 12:14:01 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# earthspace-pdx
+ 
---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function toggleIcon() {
+  $('.icon').on('click', function () {
+    $('.icon').toggleClass('active')
+    $('.menu-mobile').slideToggle(300)
+  })
+}
+
+function closeMobileMenu() {
+  $('.menu-mobile').on('click', 'a', function () {
+    $('.icon').trigger('click')
+  })
+}
