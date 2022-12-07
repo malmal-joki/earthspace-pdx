d74635125d7e2b4641574b35781fdc55e1cdb373
Mallory Joki <maljoki@pdx.edu>
2022-12-07 14:27:00 -0800

    Updated-menu-new-slideshow

--- script.js
+++ script.js
@@ -13,28 +13,31 @@ function closeMobileMenu() {
   })
 }
 
-var slideIndex = 1
+$(toggleIcon)
+$(closeMobileMenu)
 
-function plusDivs(n) {
-  showDivs((slideIndex += n))
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
-function showDivs(n) {
-  var i
-  var x = document.getElementsByClassName('mySlides')
-  if (n > x.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = x.length
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = 'none'
-  }
-  x[slideIndex - 1].style.display = 'block'
-}
 
 $(toggleIcon)
 $(closeMobileMenu)
 
-showDivs(slideIndex)
+
